cda807e5c42df6b831fe68a1556d57b4f1aa8d46
drew8687 <eloifidriss>
2023-08-10 19:16:30 +0100

    3eme commit

--- src/component/NewProducts.js
+++ src/component/NewProducts.js
@@ -7,12 +7,9 @@ function NewProducts() {
   const [checked, setChecked] = useState(false);
   const handelSaveProduct = (event) => {
     event.preventDefault();
-    let product={name,price:price,checked:checked};
-    saveProducts(product).then(resp=>{
+    let product = { name, price: price, checked: checked };
+    saveProducts(product).then((resp) => {
       alert(JSON.stringify(resp.data));
-
-
-
     });
   };
 
@@ -22,26 +19,46 @@ function NewProducts() {
         <div className="card">
           <div className='"card-body'></div>
           <div>
-          <form onSubmit={handelSaveProduct}>
-          <div class="form-group">
-            <label>Name</label>
-            <input onChange={(e)=>setName(e.target.value)}
-            value={name} 
-            type="text" class="form-control"   placeholder="Enter Name"/>
-            <small  class="form-text text-muted">nom est obligatoire</small>
-          </div>
-          <div class="form-group">
-            <label >Price</label>
-            <input onChange={(e)=>setPrice(e.target.value)} type="text" class="form-control" 
-            value={price}  placeholder="Enter Price"/>
-          </div>
-          <div class="form-check">
-          <input onChange={(e)=>setChecked(e.target.value)}
-          value={checked}  type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Check </label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+            <form onSubmit={handelSaveProduct}>
+              <div class="form-group">
+                <label>Name</label>
+                <input
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  type="text"
+                  class="form-control"
+                  placeholder="Enter Name"
+                />
+                <small class="form-text text-muted">nom est obligatoire</small>
+              </div>
+              <div class="form-group">
+                <label>Price</label>
+                <input
+                  onChange={(e) => setPrice(e.target.value)}
+                  type="text"
+                  class="form-control"
+                  value={price}
+                  placeholder="Enter Price"
+                />
+              </div>
+              <div class="form-check">
+                <input
+                  onChange={(e) => setChecked(e.target.value)}
+                  value={checked}
+                  type="checkbox"
+                  class="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1">
+                  Check{" "}
+                </label>
+              </div>
+              <div class="col-xs-10">
+                <button type="submit" class="btn btn-primary">
+                  Submit
+                </button>{" "}
+              </div>
+            </form>
           </div>
         </div>
       </div>
--- src/component/Products.js
+++ src/component/Products.js
@@ -1,14 +1,16 @@
-import { faCheckCircle, faCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect, useState } from 'react'
-import { checkProducts, deleteProduct, getProducts } from '../app/app';
-import { json } from 'react-router-dom';
-
-
-
+import {
+  faCheckCircle,
+  faCircle,
+  faSearch,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { checkProducts, deleteProduct, getProducts } from "../app/app";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
 
 function Products() {
-    /*const [products,setProducts]=useState([
+  /*const [products,setProducts]=useState([
         {id:1,name:"computer",price:4300,checked:false},
         {id:2,name:"pc",price:4200,checked:true},
         {id:3,name:"smart phone",price:5300,checked:false},
@@ -17,84 +19,142 @@ function Products() {
 
 
     ])*/
-    const [products,setProducts]=useState([]);
-    useEffect(()=>{
-        handelGetProducts();
-
-    },[]);
-    const handelGetProducts=()=>{
-        getProducts()
-        .then(resp=>{
-            setProducts(resp.data);
-         
-
-        });
-    };
-
-
-    const supprimerPrd=(product)=>{
-        deleteProduct(product).then((resp) => {
-            const newProducts=products.filter((p)=>p.id!==product.id);
-            setProducts(newProducts);
-            alert(JSON.stringify(product));
-
-
-        });
-};
-    const changer_etat=(product)=>{
-        checkProducts(product)
-        .then((resp) => {
-            const newProducts =products.map((p) => {
-                if(p.id === product.id){
-                    p.checked=!p.checked;
-                }
-                return p;
-            });
-                setProducts(newProducts);
-
-           
+  const [query, setQuery] = useState("");
+  const [state, setState] = useState({
+    products: [],
+    currentPage: 1,
+    pagesize: 4,
+    keyword: "",
+    totalPages: 0,
+  });
+  useEffect(() => {
+    handelGetProducts(state.keyword, state.currentPage, state.pagesize);
+  }, []);
+  const handelGetProducts = (keyword, page, size) => {
+    getProducts(keyword, page, size).then((resp) => {
+      const totalElements = resp.headers["x-total-count"];
+      let totalPages = Math.floor(totalElements / size);
+      if (totalElements % size !== 0) ++totalPages;
+      setState({
+        ...state,
+        products: resp.data,
+        keyword: keyword,
+        currentPage: page,
+        pagesize: size,
+        totalPages: totalPages,
+      });
     });
-   
-};
-   return (
-    <div className="p-1 m-1">
-    <div className='"row'><div className='col-md-6'>
-    <div lassName='card'>
-    <div className='"card-body'></div>
-    <table className="table">
-    <thead>
-    <tr>
-    <th>ID</th>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Checked</th>
-    </tr>
-    </thead>
-    <tbody>{
-        products.map(product=>(
-            <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.name}</td>
-            <td>{product.price}</td>
-            <td>
-            <button onClick={()=>changer_etat(product)} className='btn btn-outline-success'>
-            <FontAwesomeIcon    
-            icon={product.checked ? faCheckCircle:faCircle}
-            ></FontAwesomeIcon>
-            </button></td>
-            <td>
-            <button onClick={()=>supprimerPrd(product)} className='btn btn-outline-danger'>
-            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></button></td>
-            </tr>
+  };
+  const HandelgetPage = (page) => {
+    handelGetProducts(state.keyword, page, state.pagesize);
+  };
 
-        ))
-    }</tbody>
-    </table>
+  const supprimerPrd = (product) => {
+    deleteProduct(product).then((resp) => {
+      const newProducts = state.products.filter((p) => p.id !== product.id);
+      setState(newProducts);
+    });
+  };
+  const changer_etat = (product) => {
+    checkProducts(product).then((resp) => {
+      console.log(resp.data.JSON);
+
+      const newProducts = state.products.map((p) => {
+        if (p.id === product.id) {
+          p.checked = !p.checked;
+        }
+        return p;
+      });
+      setState(newProducts);
+    });
+  };
+  const handelSearch = (event) => {
+    event.preventDefault();
+    setState({ ...state, keyword: query });
+  };
+  return (
+    <div className="p-1 m-1">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card m-1">
+            <div className='"card-body'>
+              <form onSubmit={handelSearch} >
+                <div className=" row g-2">
+                  <div className="col-auto">
+                    <input
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                      className="form-control"
+                    ></input>
+                  </div>
+                  <div className="col-auto">
+                    <button className="btn-btn-success">
+                      <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div lassName="card">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Checked</th>
+                </tr>
+              </thead>
+              <tbody>
+                {state.products.map((product) => (
+                  <tr key={product.id}>
+                    <td>{product.id}</td>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>
+                      <button
+                        onClick={() => changer_etat(product)}
+                        className="btn btn-success"
+                      >
+                        <FontAwesomeIcon
+                          icon={product.checked ? faCheckCircle : faCircle}
+                        ></FontAwesomeIcon>
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => supprimerPrd(product)}
+                        className="btn btn-outline-danger"
+                      >
+                        <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <ul className="nav nav-pills">
+              {new Array(state.totalPages).fill(0).map((v, index) => (
+                <li>
+                  <button
+                    onClick={() => HandelgetPage(index + 1)}
+                    className={
+                      index + 1 === state.currentPage
+                        ? "btn btn-info ms-1"
+                        : "btn btn-outline-info ms-1"
+                    }
+                  >
+                    {index + 1}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    </div>
-    )
+  );
 }
 
-export default Products
+export default Products;
